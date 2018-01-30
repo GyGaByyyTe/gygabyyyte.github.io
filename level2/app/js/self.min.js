43365475725fe8a5a7b41f85bacd8469a741c945
GyGaByyyTe <gygabyyyte@gmail.com>
2018-01-31 00:46:26 +0300

    Секция бургера + слайдер

---
+++ level2/app/js/self.min.js
@@ -0,0 +1,50 @@
+var slideNow = 1;
+var slideCount = $('.carousel__wrapper').children().length;
+var translateWidth = 0;
+
+$(document).ready(function() {
+    
+    $('.carousel__btn--next').click(function() {
+        nextSlide();
+    });
+
+    $('.carousel__btn--prev').click(function() {
+        prevSlide();
+    });
+});
+
+
+function nextSlide() {
+    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+        $('.carousel__wrapper').css('transform', 'translate(0, 0)');
+        slideNow = 1;
+    } else {
+        translateWidth = -$('.carousel__viewport').width() * (slideNow);
+        $('.carousel__wrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow++;
+    }
+}
+
+function prevSlide() {
+    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+        translateWidth = -$('.carousel__viewport').width() * (slideCount - 1);
+        $('.carousel__wrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow = slideCount;
+    } else {
+        translateWidth = -$('.carousel__viewport').width() * (slideNow - 2);
+        $('.carousel__wrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow--;
+    }
+}
