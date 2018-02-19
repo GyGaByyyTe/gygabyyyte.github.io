71baa48ba62cb8dc2d91f22541e826399ebd9d81
GyGaByyyTe <gygabyyyte@gmail.com>
2018-02-19 23:42:44 +0300

    Исходники портфолио

--- portfolio/gulpfile.js
+++ portfolio/gulpfile.js
@@ -10,6 +10,7 @@ var browserSync = require("browser-sync");
 var sass = require("gulp-sass");
 var cssnano = require("gulp-cssnano");
 var rename = require("gulp-rename");
+var autoprefixer = require('gulp-autoprefixer');
 //склейка и сжатие скриптов js
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
@@ -26,6 +27,7 @@ var newer = require("gulp-newer");
 // var moreCSS = require("gulp-more-css");
 // var jade = require("gulp-jade");
 
+// --------------------------------------------- ИЗОБРАЖЕНИЯ И ШРИФТЫ
 
 // оптимизация изображений
 var image_src = "app/img/**/*";
@@ -38,12 +40,20 @@ gulp.task("imagemin", function () {
     .pipe(gulp.dest(image_dest))
 });
 
-// Compile Our Sass
+// --------------------------------------------- CSS ФОРМИРОВАНИЕ
 var css_src = "app/scss/**/*.scss";
 var css_res = "styles.min.css";
 var css_dest = "app/css";
 
-gulp.task("sass", function () {
+var vendor_css_src = [
+  "app/libs/normalize-css/normalize.css",
+];
+var vendor_css_res = "vendor.min.css";
+var vendor_css_dest = "app/css";
+
+// Compile Our Sass
+gulp.task("cssSelf", function () {
+  // все исходные стили компилятся из одного файла с импортами
   return gulp.src("app/scss/styles.scss")
     // .pipe(sourcemaps.init())
     // .pipe(debug({title:"scss"}))
@@ -56,34 +66,62 @@ gulp.task("sass", function () {
     .pipe(concat(css_res))
     .pipe(cssnano())
     // .pipe(sourcemaps.write())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest(css_dest))
     .pipe(browserSync.reload({ stream: true }));
 });
 
+// Таск для объединения и минификации CSS-файлов внешних библиотек
+gulp.task("vendorCSS", function () {
+  return gulp.src(vendor_css_src)
+    .pipe(concat(vendor_css_res))
+    .pipe(cssnano())
+    .pipe(gulp.dest(vendor_css_dest));
+});
+
+// --------------------------------------------- JavaScript JS ФОРМИРОВАНИЕ
+var vendor_js_src = [
+  "app/libs/jquery/dist/jquery.js",
+];
+var vendor_js_res = "vendor.min.js";
+var vendor_js_dest = "app/js";
+
+var js_src = "app/js/assets/**/*.js";
+var js_res = "self.min.js";
+var js_dest = "app/js";
+
+// Таск для объединения и минификации JS-файлов внешних библиотек
+gulp.task("vendorJS", function () {
+  return gulp.src(vendor_js_src)
+    .pipe(debug({ title: "js" }))
+    .pipe(concat(vendor_js_res))
+
+    .pipe(uglify())
+    .pipe(gulp.dest(vendor_js_dest))
+});
+
 // Lint Task
 gulp.task("lint", function () {
-  return gulp.src("app/js/*.js")
+  return gulp.src(js_src)
     .pipe(jshint())
     .pipe(jshint.reporter("default"));
 });
-// Concatenate & Minify JS libs
-var js_res = "libs.min.js";
-var js_dest = "app/js";
 
+// Concatenate & Minify self-JS
 gulp.task("scripts", ["lint"], function () {
-  return gulp.src([
-    // "app/libs/jquery/dist/jquery.min.js",
-    // "app/libs/magnific-popup/dist/jquery.magnific-popup.min.js"
-    "app/js/*.js"
-  ])
-    // .pipe(concat(js_res))
-    // .pipe(uglify())
+  return gulp.src(js_src)
+    .pipe(concat(js_res))
+    .pipe(uglify())
     .pipe(gulp.dest(js_dest))
     .pipe(browserSync.reload({ stream: true }));
 });
 
+// --------------------------------------------- ЛАЙВ СЕРВЕР
 //live Reload
-gulp.task("liveServer", function () {
+gulp.task("server", function () {
   browserSync({
     server: {
       baseDir: 'app'
@@ -92,22 +130,24 @@ gulp.task("liveServer", function () {
   });
 });
 
+// --------------------------------------------- ОСНОВНЫЕ ТАСКИ
+
 //Clean
 gulp.task("clean", function () {
   return del.sync("dist");
 });
 
 // Watch Files For Changes 
-gulp.task("watch", ["liveServer", "sass", "scripts"], function () {
+gulp.task("watch", ["server", "vendorCSS", "vendorJS", "cssSelf", "scripts"], function () {
   gulp.watch(css_src, function (event, cb) {
-    setTimeout(function () { gulp.start('sass'); }, 500) // задача выполниться через 500 миллисекунд и файл успеет сохраниться на диске
+    setTimeout(function () { gulp.start('cssSelf'); }, 500) // задача выполниться через 500 миллисекунд и файл успеет сохраниться на диске
   });
   gulp.watch("app/*.html", browserSync.reload);
-  gulp.watch("app/js/**/*.js", ["scripts"]);
+  gulp.watch(js_src, ["scripts"]);
 });
 
 //Build task 
-gulp.task("build", ["clean", "sass", "scripts", "imagemin"], function () {
+gulp.task("build", ["clean", "vendorCSS", "cssSelf", "vendorJS", "scripts", "imagemin"], function () {
 
   var build_css = gulp.src(["app/css/*"])
     .pipe(gulp.dest("dist/css"));
@@ -118,10 +158,13 @@ gulp.task("build", ["clean", "sass", "scripts", "imagemin"], function () {
   var build_img = gulp.src("app/img/**/*")
     .pipe(gulp.dest("dist/img"));
 
-  var build_js = gulp.src("app/js/**/*")
+  var build_js = gulp.src("app/js/*.js")
     .pipe(gulp.dest("dist/js"));
 
   var build_html = gulp.src("app/*.html")
+    .pipe(gulp.dest("dist"));
+
+  var build_html = gulp.src("app/*.php")
     .pipe(gulp.dest("dist"));
 });
 
