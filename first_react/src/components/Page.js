e1fbde62dadebacb1cdcdff84cb00c1dcfc3cae8
GyGaByyyTe <gygabyyyte@gmail.com>
2018-02-23 18:45:17 +0300

    react tutor

--- first_react/src/components/Page.js
+++ first_react/src/components/Page.js
@@ -2,20 +2,25 @@ import React, { PropTypes, Component } from 'react';
 
 export default class Page extends Component {
   onYearBtnClick(e) {
-    this.props.setYear(+e.target.innerText);
+    this.props.getPhotos(+e.target.innerText);
   }
-
   render() {
-    const { year, photos } = this.props;
+    const { year, photos, fetching } = this.props;
     return (
-      <div>
+      <div className="ib page">
         <p>
-          <button onClick={this.onYearBtnClick.bind(this)}>2016</button>
-          <button onClick={this.onYearBtnClick.bind(this)}>2015</button>
-          <button onClick={this.onYearBtnClick.bind(this)}>2014</button>
+          <button className="btn" onClick={this.onYearBtnClick.bind(this)}>
+            2016
+          </button>{' '}
+          <button className="btn" onClick={this.onYearBtnClick.bind(this)}>
+            2015
+          </button>{' '}
+          <button className="btn" onClick={this.onYearBtnClick.bind(this)}>
+            2014
+          </button>
         </p>
-        <h3>{year} год </h3>
-        <p>У тебя {photos.length} фото.</p>
+        <h3>{year} год</h3>
+        {fetching ? <p>Загрузка...</p> : <p>У тебя {photos.length} фото.</p>}
       </div>
     );
   }
@@ -24,5 +29,5 @@ export default class Page extends Component {
 Page.propTypes = {
   year: PropTypes.number.isRequired,
   photos: PropTypes.array.isRequired,
-  setYear: PropTypes.func.isRequired
+  getPhotos: PropTypes.func.isRequired
 };
--- first_react/src/containers/App.js
+++ first_react/src/containers/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import User from '../components/User';
+// import User from '../components/User';
 import Page from '../components/Page';
 import * as pageActions from '../actions/PageActions';
 
 class App extends Component {
   render() {
-    const { user, page } = this.props;
-    const { setYear } = this.props.pageActions;
+    const { page } = this.props;
+    const { getPhotos } = this.props.pageActions;
 
     return (
-      <div>
-        <User name={user.name} />
-        <Page photos={page.photos} year={page.year} setYear={setYear} />
+      <div className="row">
+        <Page
+          photos={page.photos}
+          year={page.year}
+          getPhotos={getPhotos}
+          fetching={page.fetching}
+        />
       </div>
     );
   }
@@ -21,7 +25,7 @@ class App extends Component {
 
 function mapStateToProps(state) {
   return {
-    user: state.user,
+    // user: state.user,
     page: state.page
   };
 }
